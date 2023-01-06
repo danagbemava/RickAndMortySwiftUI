cee238a4a21097a5b80f7b04d94c76e0b3ab60b9
Daniel Agbemava <danagbemava@gmail.com>
2023-01-06 21:24:41 +0000

    feat: add empty view for CharacterList

---
+++ RickAndMorty/Character/Views/CharacterListView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterListView.swift
+//  RickAndMorty
+//
+//  Created by Daniel Agbemava on 06/01/2023.
+//
+
+import SwiftUI
+
+struct CharacterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterListView()
+    }
+}

c8c66b0d1717bd2c8a002cddd19fc94dd6a6e31f
Daniel Agbemava <danagbemava@gmail.com>
2023-01-06 21:23:30 +0000

    feat: add actual views in place of text

--- RickAndMorty/ContentView.swift
+++ RickAndMorty/ContentView.swift
@@ -12,53 +12,60 @@ struct ContentView: View {
     @State var selection = 0
     
     var body: some View {
-            TabView(selection: $selection) {
-                Text("Characters")
-                    .tag(0)
-                    .tabItem {
-                        Label(title: {
-                            Text("Characters")
-                        }, icon: {
-                            Image(systemName: "person.fill")
-                        })
-                    }
-                
-                NavigationStack {
-                    EpisodeView()
-                        .navigationTitle("Episodes")
-                        
-                }
-                .tag(1)
-                .tabItem {
-                    Label(title: {
-                        Text("Episodes")
-                    }, icon: {
-                        Image(systemName: "e.circle")
-                    })
+        TabView(selection: $selection) {
+            NavigationStack {
+                CharacterListView()
+                    .navigationTitle("Characters")
             }
+            .tag(0)
+            .tabItem {
+                Label(title: {
+                    Text("Characters")
+                }, icon: {
+                    Image(systemName: "person.fill")
+                })
+            }
+            
+            NavigationStack {
+                EpisodeView()
+                    .navigationTitle("Episodes")
                 
-                NavigationStack {
-                    LocationView()
-                        .navigationTitle("Locations")
-                }.tag(2)
-                    .tabItem {
-                        Label(title: {
-                            Text("Locations")
-                        }, icon: {
-                            Image(systemName: "location.fill")
-                        })
-                    }
-                
-                Text("Compare")
-                    .tag(3)
-                    .tabItem {
-                        Label(title: {
-                            Text("Compare")
-                        }, icon: {
-                            Image(systemName: "arrow.left.arrow.right")
-                        })
-                    }
-            }.navigationTitle(getNavigationTitle())
+            }
+            .tag(1)
+            .tabItem {
+                Label(title: {
+                    Text("Episodes")
+                }, icon: {
+                    Image(systemName: "e.circle")
+                })
+            }
+            
+            NavigationStack {
+                LocationView()
+                    .navigationTitle("Locations")
+            }
+            .tag(2)
+            .tabItem {
+                Label(title: {
+                    Text("Locations")
+                }, icon: {
+                    Image(systemName: "location.fill")
+                })
+            }
+            
+            NavigationStack {
+                ComparisonView()
+                    .navigationTitle("Compare")
+            }
+            .tag(3)
+            .tabItem {
+                Label(title: {
+                    Text("Compare")
+                }, icon: {
+                    Image(systemName: "arrow.left.arrow.right")
+                })
+            }
+        }.navigationTitle(getNavigationTitle())
     }
     
     // MARK : - private functions

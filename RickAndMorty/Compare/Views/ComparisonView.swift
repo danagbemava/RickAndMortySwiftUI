b7f544247c29944db050555d41936f0f26c95d8f
Daniel Agbemava <danagbemava@gmail.com>
2023-01-06 21:22:11 +0000

    feat: add comparison view

---
+++ RickAndMorty/Compare/Views/ComparisonView.swift
@@ -0,0 +1,20 @@
+//
+//  ComparisonView.swift
+//  RickAndMorty
+//
+//  Created by Daniel Agbemava on 06/01/2023.
+//
+
+import SwiftUI
+
+struct ComparisonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComparisonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComparisonView()
+    }
+}
